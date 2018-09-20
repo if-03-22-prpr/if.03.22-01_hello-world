11b54ec2d6a3410555ed6e102bd6785c989487f0
Peter Bauer <p.bauer@htl-leonding.ac.at>
2018-09-20 11:32:51 +0200

    Block header comment is now added.

--- hello_world.c
+++ hello_world.c
@@ -1,3 +1,14 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2DHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			Hello World
+ * Author:		Peter Bauer
+ * ----------------------------------------------------------
+ * Description:
+ * Prints the unavoidable hello world into the console.
+ * ----------------------------------------------------------
+ */
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
